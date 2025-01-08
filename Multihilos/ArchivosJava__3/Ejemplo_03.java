3621db04b069d2f93f42913bf154558df6295b8f
unknown <DAM2B-28@JOVELLANOS.EDU>
2025-01-08 19:22:17 +0100

    hilos1.2

---
+++ Multihilos/ArchivosJava__3/Ejemplo_03.java
@@ -0,0 +1,33 @@
+package Multihilos.ArchivosJava__3;
+
+import java.time.Instant;
+import java.time.Duration;
+
+public class Ejemplo_03 {
+    @Override
+    public void run(){
+        long MaxSecs = 5;
+        Instant inicio = Instant.now();
+
+        while (Duration.between(inicio, Instant.now()).getSeconds()<MaxSecs) {
+            System.out.println("Todavía ejecuto");
+            System.out.println("Estamos en el segundo: " + Duration.between(inicio, Instant.now()).getSeconds());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("He terminado");
+    }
+    public static void main(String[] args) {
+        Ejemplo_03 hiloTemporal = new Ejemplo_03();
+        hiloTemporal.start();
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+}
