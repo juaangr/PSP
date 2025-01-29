7a0411c213a5027d67eda25c0ba373df668a1013
juaangr <rodriguezgomezj195@gmail.com>
2025-01-29 20:19:13 +0100

    funciona2?

--- ProgramaComRed/KahootLike/Cliente.py
+++ ProgramaComRed/KahootLike/Cliente.py
@@ -1,3 +1,20 @@
 import socket
 import threading
 import json 
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+direccion_server = ("localhost", 3000)
+
+sock.connect(direccion_server)
+
+mensaje = json.dumps({"action": "listar"})
+
+sock.send(mensaje)
+data = sock.recv(1024)
+if not data:
+    sock.close()
+    sys.exit()
+data_json = json.loads(data)
+print(data_json)
--- ProgramaComRed/KahootLike/servidor.py
+++ ProgramaComRed/KahootLike/servidor.py
@@ -1,3 +1,24 @@
 import socket
 import threading
 import json 
+
+dir_server = ("localhost", 3000)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind(dir_server)
+
+server.listen(5) # el num 5 es de ejemplo
+
+def acciones(cliente, direccion):
+    data = cliente.recv(1024)
+    if not data:
+        cliente.close()
+        print("Conexion cerrada")
+    data_json = json.loads(data)
+    print(data_json)
+
+
+while True:
+    cliente, direccion = server.accept()
+    threading.Thread(target=acciones, args=(cliente, direccion))
